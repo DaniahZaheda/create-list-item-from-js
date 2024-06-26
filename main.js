796444a3f69259fc89e6b416b33df24c6b435a6c
DaniahZaheda <daniahzaheda@gmail.com>
2024-06-26 09:05:29 +0300

    first

---
+++ main.js
@@ -0,0 +1,16 @@
+var data = "";
+        data += `<li>c++</li>`;
+        data += `<li>css</li>`;
+        data += `<li>html</li>`;
+
+        console.log(data);
+        document.querySelector(".lang").innerHTML = data;
+
+
+        var data1 ="";
+        data1 += `<li>snakcornar</li>`;
+        data1 += `<li>pizza one</li>`;
+        data1 += `<li>zuaar</li>`;
+
+        console.log(data);
+        document.querySelector(".res").innerHTML = data1;
